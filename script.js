ccba85cbc5dfe416848d60fb23fe2457c44cd121
Evan Woods <you@example.com>
2022-01-06 19:35:01 +1100

    added logic to show a value on click of a tile

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let SIZE = 24;
 
 let canvas = document.getElementById("canvas");
 
+// Function to create the buttons
 function createButtons() {
     canvas.style.width = ROWS * SIZE + "px";
     canvas.style.height = COLUMNS * SIZE + "px";
@@ -11,20 +12,29 @@ function createButtons() {
         for (let j=0; j<COLUMNS; j++) {
             let btn = document.createElement("button");
             btn.classList.add("btn");
+            btn.style.float = "left";
             btn.style.width = SIZE + "px";
             btn.style.height = SIZE + "px";
             canvas.appendChild(btn);
+
+            btn.addEventListener("click", function() {
+                btn.disabled = true;
+                updateButtons(btn);
+            });
         }
     }
 }
 
-function updateButtons() {
-    let btn = document.querySelectorAll(".btn");
+// Function to update buttons on click to show the value under it
+function updateButtons(button) {
+    if (button.disabled = true) {
+        button.textContent = 1;
+    }
 }
 
+// main function to execute the game logic
 function main() {
     createButtons();
-    updateButtons();
 }
 
 main();
